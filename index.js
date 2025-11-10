07911b37950dde01c2d46e1302940e8ba70f2d08
Teraz6 <retenk@gmail.com>
2025-11-10 16:34:19 +0200

    Add clients

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const thingamabobs = [
     {id: 3, name: "sapakas", price: 2000}
 ]
 
+const clients = [
+    {id: 1, name:"tester", email: "tester@gmail.com"}
+]
+
 app.get('/thingamabobs', (req, res) => {res.send(thingamabobs)})
 
 app.get('/thingamabobs/:id', (req, res) => {
@@ -43,5 +47,57 @@ app.delete('/thingamabobs/:id', (req, res) => {
     thingamabobs.splice(req.params.id - 1, 1)
     res.status(204).send({error: "No content"})
 })
+
+
+// Clients
+
+app.get('/clients', (req, res) => {res.send(clients)})
+
+app.get('/clients/:id', (req, res) => {
+    if (typeof clients[req.params.id - 1] === 'undefined')
+    {
+        return res.status(404).send({error: "Object not found. Check your clients id"})
+    }
+})
+
+app.post('/clients', (req, res) => {
+    if (!req.body.name || !req.body.email)
+    {
+        return res.status(400).send({error:"One or multiple parameters missing"})
+    }
+    let newClients = {
+        id: clients.length+1,
+        name: req.body.name,
+        email: req.body.email
+    }
+    clients.push(newClients)
+    res.status(201).location('localhost:8080/clients/' + (clients.length - 1)).
+    send(newClients)
+})
+
+app.delete('/clients/:id', (req, res) => {
+    if (typeof clients[req.params.id - 1] === 'undefined')
+    {
+        return res.status(404).send({error: "Object not found. Check your clients id"})
+    }
+    clients.splice(req.params.id - 1, 1)
+    res.status(204).send({error: "No content"})
+})
+
+app.put('/clients/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name, email } = req.body;
+
+    const client = clients.find(c => c.id === id);
+
+    if (!client) {
+        return res.status(404).send({ error: "Client not found. Check your client id." });
+    }
+
+    if (name) client.name = name;
+    if (email) client.email = email;
+
+    res.send(client);
+});
 
 app.listen(8080, () => {console.log(`API running at: http://localhost:8080`)})
